515441e542931654431a29c88d5ecccd1770a094
Xin Lei Lin <xinlei55555@gmail.com>
2024-09-09 20:11:28 -0400

    feat: new features

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,34 +1,50 @@
+
 /**
  * Solve the FizzBuzz challenge.
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    // this is a private class, but I'll make it public to be able to run it
+    public static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-        for (int i = 1; i < 100; i++) {
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            System.out.println("Fizz Buzz");
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+        } else if (divisibleBy3) {
 
-                System.out.println("Fizz Buzz");
+            System.out.println("Fizz");
 
-            } else if (divisibleBy3) {
+        } else if (divisibleBy5) {
 
-                System.out.println("Fizz");
+            System.out.println("Buzz");
 
-            } else if (divisibleBy5) {
+        } else {
 
-                System.out.println("Buzz");
+            System.out.println(i);
 
-            } else {
-
-                System.out.println(i);
-
-            }
         }
+        
+    }
+
+    public static void main(String[] args) {
+        /*
+        
+        for (int i = 1; i < 100; i++) {
+         */
+        int i = 1;
+        while (i < 100) {
+            doFizzBuzz(i); // this is what the step 2 task 2 is supposed to create
+            i = method(i);
+        }
+    }
+
+    private static int method(int i) {
+        i++;
+        return i;
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args){
+        // this is an import
+        FizzBuzz.doFizzBuzz(5);
+
+        // calling the main function in Main, but we need to pass parameters (String[] args)
+        FizzBuzz.main(new String[]{});
+
+    }
+}
